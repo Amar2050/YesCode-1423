d15787bd63ab06069751cc374652b6075e4182b5
Amar2050 <amarboutayeb@gmail.com>
2022-05-20 16:02:16 +0200

    TWIG path explication

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+
     #[Route('/', name: 'app_home')]
     public function index(){
         return $this->render('home/index.html.twig', []);
     }
+    #[Route('/article-list', name: 'articles')]
+    public function articles(){
+        return $this->render('home/articles.html.twig', []);
+    }
+
+
 }
