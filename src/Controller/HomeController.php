c673220d439d55bb3e7b5db843f39739621c5e5f
Amar2050 <amarboutayeb@gmail.com>
2022-05-20 14:44:47 +0200

    Avant Explication BLOCK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,30 +11,6 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(){
-
-
-        $user = new stdClass();
-        $user->isConnected = true;
-        $user->age = 28;
-        $author = "lOïS sNowden";
-        $article = new stdClass();
-        $article->title = "La théorie des fluides gastriques !";
-        $article->intro = "Tout savoir sur le bidon...";
-        $article->content = "Bla bla bla ect ect etc etc etc trop cool cool";
-
-        $games = [
-            'BF 2042' => 128, 
-            'COD 49' => 100, 
-            'FIFA 2034' => 2, 
-            'NFL 2077' => 4,
-            'NBA Mutations' => 7
-        ];
-
-        return $this->render('home/index.html.twig', [
-            "auteur" => $author,
-            "article" => $article,
-            "user"    => $user,
-            "jeux"    => $games
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
 }
