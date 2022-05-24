8edab4db70d30dce04ebd7c99fafc5a5da62c740
Amar2050 <amarboutayeb@gmail.com>
2022-05-24 17:19:55 +0200

    Clean du controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,6 @@
 
 namespace App\Controller;
 
-
-use App\Entity\Fruit;
-use App\Repository\FruitRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,50 +9,8 @@ class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'home_page')]
-    public function index(EntityManagerInterface $manager, FruitRepository $repo){
-        
-      
-        // $banane = new Fruit();
-
-        // $banane->setName("Tuplant des graines");
-        // $banane->setPoids(752); // POO setter getter
-        // $banane->setCalibre(8); // POO 
-
-        // $laurent = new Fruit();
-
-        // $laurent->setName("Michoulax");
-        // $laurent->setPoids(104);
-        // $laurent->setCalibre(50);
-        // //------------
-
-        // //------------------------
-        // $manager->persist($laurent); // 
-        // $manager->persist($banane); // 
-        // //-------------------------
-
-        // $manager->flush(); // excute ! ce qui est persist
-
-        // dump($laurent->getName());
-
-        $fruits = $repo->findAll();
-
-        foreach ($fruits as $fruit) { 
-
-            if($fruit->getId() > 5){
-                $fruit->setCalibre(2050);
-                $manager->persist($fruit);
-            }
-        }
-
-
-
- 
-        $manager->flush();
-
-      
-        
+    public function index(){
         return $this->render('home/index.html.twig', [ 
-            "fruits" => $fruits
         ]);
     }
 
