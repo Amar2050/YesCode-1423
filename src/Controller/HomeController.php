043a8c8367f8d456f2d6d47821208af81098b7b9
Amar2050 <amarboutayeb@gmail.com>
2022-05-24 17:18:55 +0200

    Exemple de jeu avec Repo et Manager

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,30 +16,44 @@ class HomeController extends AbstractController
     public function index(EntityManagerInterface $manager, FruitRepository $repo){
         
       
-        $banane = new Fruit();
+        // $banane = new Fruit();
 
-        $banane->setName("Tuplant des graines");
-        $banane->setPoids(752); // POO setter getter
-        $banane->setCalibre(8); // POO 
+        // $banane->setName("Tuplant des graines");
+        // $banane->setPoids(752); // POO setter getter
+        // $banane->setCalibre(8); // POO 
 
-        $laurent = new Fruit();
+        // $laurent = new Fruit();
 
-        $laurent->setName("Michoulax");
-        $laurent->setPoids(104);
-        $laurent->setCalibre(50);
-        //------------
+        // $laurent->setName("Michoulax");
+        // $laurent->setPoids(104);
+        // $laurent->setCalibre(50);
+        // //------------
 
-        //------------------------
-        $manager->persist($laurent); // 
-        $manager->persist($banane); // 
-        //-------------------------
+        // //------------------------
+        // $manager->persist($laurent); // 
+        // $manager->persist($banane); // 
+        // //-------------------------
 
-        $manager->flush(); // excute ! ce qui est persist
+        // $manager->flush(); // excute ! ce qui est persist
 
-        dump($laurent->getName());
+        // dump($laurent->getName());
 
         $fruits = $repo->findAll();
-        dump($fruits);
+
+        foreach ($fruits as $fruit) { 
+
+            if($fruit->getId() > 5){
+                $fruit->setCalibre(2050);
+                $manager->persist($fruit);
+            }
+        }
+
+
+
+ 
+        $manager->flush();
+
+      
         
         return $this->render('home/index.html.twig', [ 
             "fruits" => $fruits
