c946a2da938e7230e55c0522b37fbe3de11c33bd
Amar2050 <amarboutayeb@gmail.com>
2022-05-31 16:39:12 +0200

    Avant FINAL - TP

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
     #[Route('/articles', name: 'articles_index')]
-    public function index(ArticleRepository $articleRepository): Response
-    {
-        return $this->render('article/index.html.twig', [
-            "articles" => $articleRepository->findAll(),
+    public function index(ArticleRepository $articleRepository): Response{
+        return $this->render('article/index.html.twig', ["articles" => $articleRepository->findAll()]);
+    }
+    
+    #[Route('/articles/new', name: 'article_create')]
+    public function create(Request $request, EntityManagerInterface $manager){
+
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        // dump($article);
+
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            
+            $manager->persist($article);
+            $manager->flush();
+            
+            $this->addFlash("success", "L'article <strong>{$article->getTitle()}</strong> a bien été crée");
+
+            return $this->redirectToRoute('article_show', ['slug' => $article->getSlug() ]);
+        }
+
+        return $this->render('article/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
+
+    #[Route('/articles/{slug}', name: 'article_show')]
+    public function show($slug, ArticleRepository $articleRepository){
+        $article = $articleRepository->findOneBySlug($slug);
+        return $this->render('article/show.html.twig', ["article" => $article,]);
+    }
+    
+
+
 }
